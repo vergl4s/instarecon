5a529b5520a39caf6adf18859aba385dca826b91
vergl4s <lfbteixeira@gmail.com>
2015-04-21 19:55:05 +1000

    More specific exceptions for dns.query and ipaddress stuff

---
+++ tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import unittest
+
+class TestHostMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.host = Host('google.com') 
+                
+    # def test_upper(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
